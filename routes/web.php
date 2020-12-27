21d3b0541d68e571625fe1ea8be5a876974d846d
shiro ibi <pres@sunyou.co.jp>
2020-12-28 06:37:52 +0900

    ルーティング勉強

--- routes/web.php
+++ routes/web.php
@@ -24,10 +24,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('tt', function () {
-    return view('tt');
-});
 
 Route::get('direct', function () use ($html) {
+    return $html;
+});
+
+
+Route::get('hello/{msg}', function ($msg) {
+    $html = <<<EOF
+<html>
+<head>
+    <title>Hello Shiro!</title>
+    <style>
+        body {font-size:20pt; color:#999;}
+        h1 {font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px;}
+    </style>
+</head>
+<body>
+    <h1>hello</h1>
+    <p>{$msg}</p>
+    <p>sample page</p>
+</body>
+</html>
+EOF;
+
     return $html;
 });
