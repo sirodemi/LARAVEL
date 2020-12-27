e952f6987b94903fe26045ae2eac143ac04b634c
shiro ibi <pres@sunyou.co.jp>
2020-12-28 06:44:16 +0900

    routing ok

--- routes/web.php
+++ routes/web.php
@@ -32,21 +32,21 @@ Route::get('direct', function () use ($html) {
 
 Route::get('hello/{msg}', function ($msg) {
     $html = <<<EOF
-<html>
-<head>
-    <title>Hello Shiro!</title>
-    <style>
-        body {font-size:20pt; color:#999;}
-        h1 {font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px;}
-    </style>
-</head>
-<body>
-    <h1>hello</h1>
-    <p>{$msg}</p>
-    <p>sample page</p>
-</body>
-</html>
-EOF;
+    <html>
+    <head>
+        <title>Hello Shiro!</title>
+        <style>
+            body {font-size:20pt; color:#999;}
+            h1 {font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px;}
+        </style>
+    </head>
+    <body>
+        <h1>hello</h1>
+        <p>{$msg}</p>
+        <p>sample page</p>
+    </body>
+    </html>
+    EOF;
 
     return $html;
 });
