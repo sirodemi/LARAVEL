369e1f2e3861729d7c7550225470cc58ea5ac442
衣斐司郎 <shiro@sunyou.co.jp>
2020-12-29 17:13:43 +0900

    DB insert

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -13,4 +13,30 @@ class HelloController extends Controller
         $items = DB::select('select * from people');
         return view('hello.index', ['items' => $items]);
     }
+
+    public function post(Request $request)
+    {
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
+    }
+
+
+    public function add(Request $request)
+    {
+        return view('hello.add');
+    }
+
+
+    public function create(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+
+        DB::insert('insert into people (name,mail,age) values (:name, :mail,:age)', $param);
+        return redirect('/hello');
+        // return redirect('/laravel6/public/hello');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,9 @@ Route::get('/', function () {
 
 Route::get('hello', 'HelloController@index');
 
+Route::get('hello/add', 'HelloController@add');
+Route::post('hello/add', 'HelloController@create');
+
 // Route::get('hello', function () {
 //     return ('----');
 // });
