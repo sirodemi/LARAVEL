05f95ee67e01becb5150416c019d01c355f34f85
衣斐司郎 <shiro@sunyou.co.jp>
2020-12-29 18:03:54 +0900

    DB edit

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ Route::get('hello', 'HelloController@index');
 Route::get('hello/add', 'HelloController@add');
 Route::post('hello/add', 'HelloController@create');
 
+Route::get('hello/edit', 'HelloController@edit');
+Route::post('hello/edit', 'HelloController@update');
+
 // Route::get('hello', function () {
 //     return ('----');
 // });
