a008775022e71e1b890e2612bb54365d07974a7e
衣斐司郎 <shiro@sunyou.co.jp>
2020-12-29 20:50:46 +0900

    クエリを使わずに

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -69,45 +69,65 @@ class HelloController extends Controller
             'age' => $request->age,
         ];
 
-        DB::insert('insert into people (name,mail,age) values (:name, :mail,:age)', $param);
+        DB::table('people')->insert($param);
+        // DB::insert('insert into people (name,mail,age) values (:name, :mail,:age)', $param);
         return redirect('/hello');
     }
 
     public function edit(Request $request)
     {
-        $param = ['id' => $request->id];
-        $item = DB::select('select * from people where id = :id', $param);
-        return view('hello.edit', ['form' => $item[0]]);
+        $item = DB::table('people')
+            ->where('id', $request->id)->first();
+        return view('hello.edit', ['form' => $item]);
+        // $param = ['id' => $request->id];
+        // $item = DB::select('select * from people where id = :id', $param);
+        // return view('hello.edit', ['form' => $item[0]]);
     }
 
 
     public function update(Request $request)
     {
         $param = [
-            'id' => $request->id,
             'name' => $request->name,
             'mail' => $request->mail,
             'age' => $request->age,
         ];
+        DB::table('people')
+            ->where('id', $request->id)
+            ->update($param);
 
-        DB::update('update people set name=:name, mail=:mail, age=:age where id= :id', $param);
+
+        // $param = [
+        //     'id' => $request->id,
+        //     'name' => $request->name,
+        //     'mail' => $request->mail,
+        //     'age' => $request->age,
+        // ];
+        // DB::update('update people set name=:name, mail=:mail, age=:age where id= :id', $param);
         return redirect('/hello');
     }
 
 
     public function del(Request $request)
     {
-        $param = ['id' => $request->id];
-        $item = DB::select('select * from people where id = :id', $param);
-        return view('hello.del', ['form' => $item[0]]);
+        $item = DB::table('people')
+            ->where('id', $request->id)->first();
+        return view('hello.del', ['form' => $item]);
+
+        // $param = ['id' => $request->id];
+        // $item = DB::select('select * from people where id = :id', $param);
+        // return view('hello.del', ['form' => $item[0]]);
     }
 
 
     public function remove(Request $request)
     {
-        $param = ['id' => $request->id];
-
-        DB::delete('delete from people where id= :id', $param);
+        DB::table('people')
+            ->where('id', $request->id)->delete();
         return redirect('/hello');
+
+        // $param = ['id' => $request->id];
+        // DB::delete('delete from people where id= :id', $param);
+        // return redirect('/hello');
     }
 }
