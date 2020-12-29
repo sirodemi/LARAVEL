9f451a80ceb77ee255a94b1f8b63c6d5a2dcd463
衣斐司郎 <shiro@sunyou.co.jp>
2020-12-29 18:48:01 +0900

    DB delete

---
+++ resources/views/hello/del.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.helloapp')
+@section('title','Delete')
+
+@section('menubar')
+    @parent
+    削除ページ
+@endsection
+
+@section('content')
+    <form action="/laravel6/public/hello/del" method="post">
+    <table>
+        @csrf
+                <input type="hidden" name="id" value="{{$form->id}}">
+        <tr><th>name:</th><td>{{$form->name}}</td></tr>
+        <tr><th>mail:</th><td>{{$form->mail}}</td></tr>
+        <tr><th>age:</th><td>{{$form->age}}</td></tr>
+        <tr><th></th><td><input type="submit" value = "send"></td></tr>
+    </table>
+    </form>
+@endsection
+
+@section('footer')
+    copyright 2020 Shiro
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::post('hello/add', 'HelloController@create');
 Route::get('hello/edit', 'HelloController@edit');
 Route::post('hello/edit', 'HelloController@update');
 
+Route::get('hello/del', 'HelloController@del');
+Route::post('hello/del', 'HelloController@remove');
+
 // Route::get('hello', function () {
 //     return ('----');
 // });
